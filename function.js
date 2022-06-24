6f6d7ac58c05b11a4167a4fec8b4ad858dc60d35
GammaMecatrónica <99369502+GammaMecatronica@users.noreply.github.com>
2022-06-24 15:46:18 -0500

    Create function.js

---
+++ function.js
@@ -0,0 +1,17 @@
+// Funcion declarada
+function sumar( n1, n2){
+    return n1+n2;
+}
+
+// Función expresada
+const sumar = function(n1, n2){
+    return n1+n2;
+}
+
+// Arrow function
+const sumar= (n1,n2)=>{
+    return n1+n2;
+}
+
+// Arrow function- Retorno implícito
+const sumar= (n1,n2) => n1+n2;
